5a17e6524c1b96a43a5f12b22284ea3409a29059
Bartłomiej Banaś <62613502+Bartasus1@users.noreply.github.com>
2023-04-14 19:38:56 +0200

    Small code changes

--- Plugins/TerrainGenerator/Source/TerrainGenerator/Private/TerrainComponent.cpp
+++ Plugins/TerrainGenerator/Source/TerrainGenerator/Private/TerrainComponent.cpp
@@ -20,58 +20,64 @@ UTerrainComponent::UTerrainComponent()
 
 void UTerrainComponent::GenerateLandmass()
 {
-	if(GetWorld())
+	if(!GetWorld())
 	{
-		if (ALandscape* Landscape = Cast<ALandscape>(GetOwner()))
+		return;
+	}
+	
+	if (ALandscape* Landscape = Cast<ALandscape>(GetOwner()))
+	{
+		ParallelFor(SampleRect.Width() * SampleRect.Height(), [&](int32 F)
 		{
-			ParallelFor(SampleRect.Width() * SampleRect.Height(), [&](int32 F)
-			{
-				HeightData[F] = SHRT_MAX;
-				
-				for(FTerrainWeights TerrainWeight : TerrainModifierWeights)
-				{
-					if(UTerrainModifier* TerrainModifier = TerrainWeight.TerrainModifier)
-					{
-						int32 i = F % SampleRect.Width() + TerrainModifier->Seed;
-						int32 j = F / SampleRect.Height() + TerrainModifier->Seed;
-					
-						float X = i / static_cast<float>(SampleRect.Width());
-						float Y = j / static_cast<float>(SampleRect.Height());
-
-				
-						const float Height = TerrainModifier->GetTerrainHeightValue(X, Y) * TerrainWeight.Weight;
-					
-						HeightData[F] = FMath::Clamp( HeightData[F] + TerrainModifier->Elevation * Height, 0, UINT16_MAX);
-
-						if(bGenerateAdditionalHeightmap)
-						{
-							const FColor Color = FLinearColor::LerpUsingHSV(FLinearColor(FColor::Black), FLinearColor(FColor::White),(Height + 1) * 0.5).ToFColor(false);
-					
-							Pixels[(F * 4) + 0] = Color.R;
-							Pixels[(F * 4) + 1] = Color.G;
-							Pixels[(F * 4) + 2] = Color.B;
-							Pixels[(F * 4) + 3] = 255;
-						}
-					
-						MaxHeight = FMath::Max(MaxHeight, TerrainModifier->Elevation);
-					}
-				}
-			});
-
-			if(ParameterCollection)
-			{
-				UMaterialParameterCollectionInstance* ParameterCollectionInstance = GetWorld()->GetParameterCollectionInstance(ParameterCollection);
-				ParameterCollectionInstance->SetScalarParameterValue("MaxHeight", MaxHeight);
-			}
+			HeightData[F] = SHRT_MAX;
 			
+			for(FTerrainWeights TerrainWeight : TerrainModifierWeights)
+			{
+				if(UTerrainModifier* TerrainModifier = TerrainWeight.TerrainModifier)
+				{
+					int32 i = F % SampleRect.Width() + TerrainModifier->Seed;
+					int32 j = F / SampleRect.Height() + TerrainModifier->Seed;
+				
+					float X = i / static_cast<float>(SampleRect.Width());
+					float Y = j / static_cast<float>(SampleRect.Height());
+
+			
+					const float Height = TerrainModifier->GetTerrainHeightValue(X, Y) * TerrainWeight.Weight;
+				
+					HeightData[F] = FMath::Clamp( HeightData[F] + TerrainModifier->Elevation * Height, 0, UINT16_MAX);
+
+					if(bGenerateAdditionalHeightmap)
+					{
+						const FColor Color = FLinearColor::LerpUsingHSV(FLinearColor(FColor::Black), FLinearColor(FColor::White),(Height + 1) * 0.5).ToFColor(false);
+				
+						Pixels[(F * 4) + 0] = Color.R;
+						Pixels[(F * 4) + 1] = Color.G;
+						Pixels[(F * 4) + 2] = Color.B;
+						Pixels[(F * 4) + 3] = 255;
+					}
+				
+					MaxHeight = FMath::Max(MaxHeight, TerrainModifier->Elevation);
+				}
+			}
+		});
+
+		if(ParameterCollection)
+		{
+			UMaterialParameterCollectionInstance* ParameterCollectionInstance = GetWorld()->GetParameterCollectionInstance(ParameterCollection);
+			ParameterCollectionInstance->SetScalarParameterValue("MaxHeight", MaxHeight);
+		}
+
+		if(bGenerateAdditionalHeightmap)
+		{
 			//Save height to the texture
 			WriteHeightDataToTexture(Pixels, SampleRect, Landscape->GetActorNameOrLabel());
-		
-			//Apply height to the landscape
-			FHeightmapAccessor<true> HeightmapAccessor(Landscape->GetLandscapeInfo());
-			HeightmapAccessor.SetData(SampleRect.Min.X, SampleRect.Min.Y, SampleRect.Width() - 1, SampleRect.Height() - 1, HeightData.GetData());
 		}
+	
+		//Apply height to the landscape
+		FHeightmapAccessor<true> HeightmapAccessor(Landscape->GetLandscapeInfo());
+		HeightmapAccessor.SetData(SampleRect.Min.X, SampleRect.Min.Y, SampleRect.Width() - 1, SampleRect.Height() - 1, HeightData.GetData());
 	}
+	
 }
 
 // Called when the game starts
@@ -86,50 +92,47 @@ void UTerrainComponent::BeginPlay()
 
 bool UTerrainComponent::WriteHeightDataToTexture(TArray<uint8>& Data, FIntRect Rect, FString LandscapeActorName)
 {
-	if(bGenerateAdditionalHeightmap)
-	{
-		FString Path = "/TerrainGenerator/";
-		FString TextureName = "HeightMap_" + LandscapeActorName;
 
-		UPackage* Package = CreatePackage(*(Path + TextureName));
-		Package->FullyLoad();
+	FString Path = "/TerrainGenerator/";
+	FString TextureName = "HeightMap_" + LandscapeActorName;
 
-		UTexture2D* NewTexture = NewObject<UTexture2D>(Package, *TextureName, RF_Public | RF_Standalone | RF_MarkAsRootSet);
-		NewTexture->AddToRoot(); // This line prevents garbage collection of the texture
-		NewTexture->SetPlatformData(new FTexturePlatformData()); // Then we initialize the PlatformData
-		NewTexture->GetPlatformData()->SizeX = Rect.Width();
-		NewTexture->GetPlatformData()->SizeY = Rect.Height();
-		NewTexture->GetPlatformData()->SetNumSlices(1);
-		NewTexture->GetPlatformData()->PixelFormat = PF_R8G8B8A8;
+	UPackage* Package = CreatePackage(*(Path + TextureName));
+	Package->FullyLoad();
 
-		FTexture2DMipMap* Mip = new FTexture2DMipMap();
-		NewTexture->GetPlatformData()->Mips.Add(Mip);
-		Mip->SizeX = Rect.Width();
-		Mip->SizeY = Rect.Height();
+	UTexture2D* NewTexture = NewObject<UTexture2D>(Package, *TextureName, RF_Public | RF_Standalone | RF_MarkAsRootSet);
+	NewTexture->AddToRoot(); // This line prevents garbage collection of the texture
+	NewTexture->SetPlatformData(new FTexturePlatformData()); // Then we initialize the PlatformData
+	NewTexture->GetPlatformData()->SizeX = Rect.Width();
+	NewTexture->GetPlatformData()->SizeY = Rect.Height();
+	NewTexture->GetPlatformData()->SetNumSlices(1);
+	NewTexture->GetPlatformData()->PixelFormat = PF_R8G8B8A8;
 
-		// Lock the texture so it can be modified
-		Mip->BulkData.Lock(LOCK_READ_WRITE);
-		uint8* TextureData = (uint8*)Mip->BulkData.Realloc(Data.Num());
-		FMemory::Memcpy(TextureData, Data.GetData(), sizeof(uint8) * Data.Num());
-		Mip->BulkData.Unlock();
+	FTexture2DMipMap* Mip = new FTexture2DMipMap();
+	NewTexture->GetPlatformData()->Mips.Add(Mip);
+	Mip->SizeX = Rect.Width();
+	Mip->SizeY = Rect.Height();
 
-		NewTexture->Source.Init(Rect.Width(), Rect.Height(), 1, 1, TSF_RGBA8, Data.GetData());
+	// Lock the texture so it can be modified
+	Mip->BulkData.Lock(LOCK_READ_WRITE);
+	uint8* TextureData = (uint8*)Mip->BulkData.Realloc(Data.Num());
+	FMemory::Memcpy(TextureData, Data.GetData(), sizeof(uint8) * Data.Num());
+	Mip->BulkData.Unlock();
 
-		NewTexture->UpdateResource();
-		Package->MarkPackageDirty();
-		FAssetRegistryModule::AssetCreated(NewTexture);
+	NewTexture->Source.Init(Rect.Width(), Rect.Height(), 1, 1, TSF_RGBA8, Data.GetData());
 
-		FString PackageFileName = FPackageName::LongPackageNameToFilename(Path, FPackageName::GetAssetPackageExtension());
-		FSavePackageArgs SavePackageArgs;
-		SavePackageArgs.SaveFlags = RF_Public | RF_Standalone | RF_NeedLoad;
-		SavePackageArgs.Error = GError;
-		SavePackageArgs.bForceByteSwapping = true;
-		SavePackageArgs.bWarnOfLongFilename = SAVE_NoError;
+	NewTexture->UpdateResource();
+	Package->MarkPackageDirty();
+	FAssetRegistryModule::AssetCreated(NewTexture);
 
-		return UPackage::SavePackage(Package, NewTexture, *PackageFileName, SavePackageArgs);
-	}
+	FString PackageFileName = FPackageName::LongPackageNameToFilename(Path, FPackageName::GetAssetPackageExtension());
+	FSavePackageArgs SavePackageArgs;
+	SavePackageArgs.SaveFlags = RF_Public | RF_Standalone | RF_NeedLoad;
+	SavePackageArgs.Error = GError;
+	SavePackageArgs.bForceByteSwapping = true;
+	SavePackageArgs.bWarnOfLongFilename = SAVE_NoError;
 
-	return false;
+	return UPackage::SavePackage(Package, NewTexture, *PackageFileName, SavePackageArgs);
+
 }
 
 void UTerrainComponent::OnRegister()
